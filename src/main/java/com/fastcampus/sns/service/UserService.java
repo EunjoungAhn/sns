7f5f3ebc5c14ae274a010b2352d4b5baacfa399e
EunjoungAhn <aej9300@gmail.com>
2022-10-10 20:23:33 +0900

    알람 테스트 작성

--- src/main/java/com/fastcampus/sns/service/UserService.java
+++ src/main/java/com/fastcampus/sns/service/UserService.java
@@ -8,6 +8,8 @@ import com.fastcampus.sns.repository.UserEntityRepository;
 import com.fastcampus.sns.util.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -61,4 +63,10 @@ public class UserService {
 
         return token;
     }
+
+    // TODO: alarm return
+    public Page<Void> alarmList(String userName, Pageable pageable){
+        return Page.empty();
+    }
+
 }
