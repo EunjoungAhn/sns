c854ada2f0f330db01062f95791f287b815b29d9
EunjoungAhn <aej9300@gmail.com>
2022-10-11 16:53:54 +0900

    코드 최적화: Alarm 호출시 User 호출 한번 줄이기

--- src/main/java/com/fastcampus/sns/service/UserService.java
+++ src/main/java/com/fastcampus/sns/service/UserService.java
@@ -69,10 +69,9 @@ public class UserService {
         return token;
     }
 
-    public Page<Alarm> alarmList(String userName, Pageable pageable){
+    public Page<Alarm> alarmList(Integer userId, Pageable pageable){
         // 회원가입 여부 체크
-        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
-        return alarmEntityRepository.findAllByUser(userEntity, pageable).map(Alarm::fromEntity);
+        return alarmEntityRepository.findAllByUserId(userId, pageable).map(Alarm::fromEntity);
     }
 
 }
